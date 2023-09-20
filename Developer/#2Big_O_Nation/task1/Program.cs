c9818adabcd827c8b88ce682a334e4a9654e5b17
Nikita <Wekly1@gmail.com>
2023-09-20 13:23:04 +0300

    Completed 2d day of  Bootcamp

---
+++ Developer/#2Big_O_Nation/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[5];
+for (int i = 0; i < 5; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("[" + string.Join(" ", array) + "]");
+Console.WriteLine(array[3]); // Выполнили 1 дествие, чтобы узнать какое число под индексом 3 в массиве
+// Сложность алгоритма О(1)
+
+// Сгенерировали массив [4, 5, 3, 1, 2]
+// O(n)
+// [1, 2, 3, 4, 5] -> O(n * log n)
+// ((5 + 1)/2) * 5 -> O(1)
+// n < n * log(n) + 1
+int summa = 0;
+for(int i = 0; i < array.Length; i++)
+summa += array[i];
+Console.WriteLine(summa);
---
+++ Developer/#2Big_O_Nation/task2/Program.cs
@@ -0,0 +1,37 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+// O(n^2)
+// for (int i = 1; i <= n; i++)
+// {
+//     for (int j = 1; j <= n; j++)
+//     {
+//         Console.Write(i * j);
+//         Console.Write("\t");
+//     }
+//     Console.WriteLine();
+// }
+
+// Сокращаем время работы программы
+// int[,] matrix = new int[n, n];
+// for (int i = 1; i <= n / 2; i++)
+// {
+//     if (i + j <= n / 2)
+//     {
+//         matrix[i, j] = i * j;
+//         matrix[j, i] = i * j;
+//     }
+//     Console.WriteLine();
+// }
+// //O(n^2 / 2)
+
+int[,] matrix = new int[n, n];
+for (int i = 1; i < n / 2; i++)
+{
+    for (i = j; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * j * (j + 1);
+        matrix[j, i] = (i + 1) * j * (j + 1);
+    }
+    Console.WriteLine();
+}
+//O(n^2 / 2)
+
